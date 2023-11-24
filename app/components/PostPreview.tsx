6e3d1b82458af00fc527d035c17ea425f603e6f6
Davud Kaplan <14043994+davudk@users.noreply.github.com>
2023-11-24 18:34:21 -0500

    Update PostPreview.tsx

--- app/components/PostPreview.tsx
+++ app/components/PostPreview.tsx
@@ -5,9 +5,11 @@ export function PostPreview(props: { post: Post }) {
 
     return (
         <a className="group flex flex-col sm:flex-row" href={`/${post.slug}`}>
-            <img className="max-h-40 sm:w-48 sm:max-h-auto"
-                src={post.featureImage}
-                alt={post.title} />
+            <div className="max-h-40 sm:w-48 sm:max-h-auto bg-slate-300">
+                {post.featureImage && (
+                    <img src={post.featureImage} alt={post.title} />
+                )}
+            </div>
             <div className="mt-1 mb-3 sm:my-0 sm:ml-3 flex-1">
                 <h2 className="text-lg group-hover:underline">{post.title}</h2>
                 <div className="text-xs text-slate-600"><time dateTime={post.published}>{post.published}</time>
